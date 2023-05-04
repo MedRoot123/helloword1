34d2a5c99c250d15c246fccec3ff7110a7190687
Taoufikchaouech <mohamedtaoufikchaouech@gmail.com>
2023-05-04 15:08:44 +0100

    Verfication classe java

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Service_Tech
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("salut med taoufik");
+    }
+}
